813e0fb47767ade477e0a19324917bc5b132a4b6
tashaljordan-dev <tashaljordan@gmail.com>
2026-05-06 19:55:14 -0500

    5/6

--- src/main/java/org/example/BetterStudentManager.java
+++ src/main/java/org/example/BetterStudentManager.java
@@ -1,56 +1,42 @@
 package org.example;
 
-// In a separate (adjacent) file, create a Student class with two public members:
-// - A String member called name
-// - A double member called grade
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class BetterStudentManager {
+
     public List<Student> students = new ArrayList<>();
 
     public void addStudent(String name, double grade) {
-        // This method should add a new student to the list of students
-    Student s = new Student();
-    s.name = name;
-    s.grade = grade; 
-    students.add(s);
+
+        Student student = new Student();
+
+        student.name = name;
+        student.grade = grade;
+
+        students.add(student);
     }
 
     public void removeStudent(String name) {
-        int indexToRemove = -1; 
 
-        for (int i =0; i < students.size(); i++) {
+        for (int i = 0; i < students.size(); i++) {
+
             if (students.get(i).name.equals(name)) {
-                indexToRemove = i;
+                students.remove(i);
                 break;
             }
-        }
-        // This method should remove the student with the given name from the list of students
-        if (indexToRemove !=-1){
-            students.remove(indexToRemove);
         }
     }
 
     public String getStudentList() {
-        // This method should return a string with each student's name and grade
-        // separated by a space, with each student separated by a newline character ('\n')
-        // E.g.
-        // John 90.9
-        // Jane 88.3
-        //
-        // You will need to use a String or StringBuilder to build the result by
-        // looping through the list of students
-        //
-        // Replace te following line with your implementation
-        StringBuilder sb = new StringBuilder();
-        for (Student s : students) {
-            sb.append(s.name)
-            .append(" ")
-            .append(s.grade)
-            .append("`n"); 
+
+        StringBuilder result = new StringBuilder();
+
+        for (Student student : students) {
+
+            result.append(student.name).append(" ").append(student.grade).append("\n");
         }
-        return sb.toString().trim();
+
+        return result.toString();
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Student {
+    public String name;
+    public double grade;
+
+}
